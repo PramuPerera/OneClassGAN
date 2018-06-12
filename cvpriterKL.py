c566a0a0a995659ad748c8e26c71d082eaefa60f
Perera <pramudi@8c8590432a82.ant.amazon.com>
2018-06-12 15:57:33 -0400

    tab issue resolved

--- cvpriterKL.py
+++ cvpriterKL.py
@@ -58,7 +58,7 @@ def set_network(depth, ctx, lr, beta1, ndf, ngf, append=True, solver='adam'):
     if append:
         netD = models.Discriminator(in_channels=6, n_layers =2 , ndf=ndf)##netG = models.CEGenerator(in_channels=3, n_layers=depth, ndf=ngf)  # UnetGenerator(in_channels=3, num_downs=8) #
     else:
-	netD = models.Discriminator(in_channels=3, n_layers =2 , ndf=ndf)
+    netD = models.Discriminator(in_channels=3, n_layers =2 , ndf=ndf)
     #netG = models.UnetGenerator(in_channels=3, num_downs =depth, ngf=ngf)  # UnetGenerator(in_channels=3, num_downs=8) #
     #netD = models.Discriminator(in_channels=6, n_layers =depth-1, ndf=ngf/4)
     netEn = models.Encoder(in_channels=3, n_layers =depth, ndf=ngf)
@@ -69,14 +69,14 @@ def set_network(depth, ctx, lr, beta1, ndf, ngf, append=True, solver='adam'):
     models.network_init(netDe, ctx=ctx)
     models.network_init(netD, ctx=ctx)
     if solver=='adam':
-	    # trainer for the generator and the discriminator
-	    trainerEn = gluon.Trainer(netEn.collect_params(), 'adam', {'learning_rate': lr, 'beta1': beta1})
+            # trainer for the generator and the discriminator
+	        trainerEn = gluon.Trainer(netEn.collect_params(), 'adam', {'learning_rate': lr, 'beta1': beta1})
             trainerDe = gluon.Trainer(netDe.collect_params(), 'adam', {'learning_rate': lr, 'beta1': beta1})
-	    trainerD = gluon.Trainer(netD.collect_params(), 'adam', {'learning_rate': lr, 'beta1': beta1})
+            trainerD = gluon.Trainer(netD.collect_params(), 'adam', {'learning_rate': lr, 'beta1': beta1})
     elif solver == 'sgd':
-	    print('sgd')
- 	    trainerG = gluon.Trainer(netG.collect_params(), 'sgd', {'learning_rate': lr, 'momentum': 0.9} )
-	    trainerD = gluon.Trainer(netD.collect_params(), 'sgd', {'learning_rate': lr, 'momentum': 0.9})
+            print('sgd')
+            trainerG = gluon.Trainer(netG.collect_params(), 'sgd', {'learning_rate': lr, 'momentum': 0.9} )
+            trainerD = gluon.Trainer(netD.collect_params(), 'sgd', {'learning_rate': lr, 'momentum': 0.9})
     return netEn, netDe, netD, trainerEn, trainerDe, trainerD
 
 def facc(label, pred):
@@ -102,39 +102,12 @@ def train(pool_size, epochs, train_data, val_data,  ctx, netEn, netDe,  netD, tr
     logging.basicConfig(level=logging.DEBUG)
     for epoch in range(epochs):
 
-     if useAE:
-	for batch in train_data:
-		train_data.reset()
-		real_in = batch.data[0].as_in_context(ctx)
-        	real_out = batch.data[1].as_in_context(ctx)
-		fake_out = netG(real_in)
-		loss = L1_loss(real_out, fake_out)
-		loss.backward()
-		trainerG.step(batch.data[0].shape[0])
-		metric2.update([real_out, ], [fake_out, ])
-	        if epoch%10 ==0:
-        	    	 filename = "checkpoints/"+expname+"_"+str(epoch)+"_G.params"
-			 netG.save_params(filename)
-            		 fake_img1 = nd.concat(real_in[0],real_out[0], fake_out[0], dim=1)
-            		 fake_img2 = nd.concat(real_in[1],real_out[1], fake_out[1], dim=1)
-            		 fake_img3 = nd.concat(real_in[2],real_out[2], fake_out[2], dim=1)
-            		 fake_img4 = nd.concat(real_in[3],real_out[3], fake_out[3], dim=1)
-			 #fake_img4T = nd.concat(real_in[3],real_out[3], fake_out[3], dim=1)
-            		 fake_img = nd.concat(fake_img1,fake_img2, fake_img3,dim=2)
-            		 visual.visualize(fake_img)
-            		 plt.savefig('outputs/'+expname+'_'+str(epoch)+'.png')
-        train_data.reset()
-	name, acc = metric.get()
-        metric2.reset()
-	print("training acc: "+ acc)
-
-     else:
         tic = time.time()
         btic = time.time()
         train_data.reset()
         iter = 0
         #print('learning rate : '+str(trainerD.learning_rate ))
-	for batch in train_data:
+	    for batch in train_data:
             ############################
             # (1) Update D network: maximize log(D(x, y)) + log(1 - D(x, G(x, z)))
             ###########################
@@ -143,7 +116,7 @@ def train(pool_size, epochs, train_data, val_data,  ctx, netEn, netDe,  netD, tr
             soft_zero = 1e-10
             fake_latent= netEn(real_in)
             mu_lv = nd.split(fake_latent, axis=1, num_outputs=2)
-       	    mu = mu_lv[0]
+            mu = mu_lv[0]
             lv = mu_lv[1]
             eps = nd.random_normal(loc=0, scale=1, shape=(batch_size, 4096), ctx=model_ctx)
             z = mu + nd.exp(0.5*lv)*eps
@@ -175,9 +148,7 @@ def train(pool_size, epochs, train_data, val_data,  ctx, netEn, netDe,  netD, tr
             ###########################
             with autograd.record():
 
-
-
-		fake_latent= netEn(real_in)
+                fake_latent= netEn(real_in)
                 mu_lv = mx.split(fake_latent, axis=1, num_outputs=2)
                 mu = mu_lv[0]
                 lv = mu_lv[1]
@@ -196,23 +167,23 @@ def train(pool_size, epochs, train_data, val_data,  ctx, netEn, netDe,  netD, tr
                 errR = logloss#L1_loss(real_out, fake_out)
                 errG.backward()
 	   
-            trainerG.step(batch.data[0].shape[0])
-            loss_rec_G.append(nd.mean(errG).asscalar()-nd.mean(errR).asscalar()*lambda1)
-            loss_rec_D.append(nd.mean(errD).asscalar())
-            loss_rec_R.append(nd.mean(errR).asscalar())
-            name, acc = metric.get()
-	    acc_rec.append(acc)
-            # Print log infomation every ten batches
-            if iter % 10 == 0:
+        trainerG.step(batch.data[0].shape[0])
+        loss_rec_G.append(nd.mean(errG).asscalar()-nd.mean(errR).asscalar()*lambda1)
+        loss_rec_D.append(nd.mean(errD).asscalar())
+        loss_rec_R.append(nd.mean(errR).asscalar())
+        name, acc = metric.get()
+        acc_rec.append(acc)
+        # Print log infomation every ten batches
+        if iter % 10 == 0:
                 name, acc = metric.get()
                 logging.info('speed: {} samples/s'.format(batch_size / (time.time() - btic)))
                 #print(errD)
-		logging.info(
+		        logging.info(
                     'discriminator loss = %f, generator loss = %f, binary training acc = %f reconstruction error= %f at iter %d epoch %d'
                     % (nd.mean(errD).asscalar(),
                        nd.mean(errG).asscalar(), acc,nd.mean(errR).asscalar() ,iter, epoch))
-            iter = iter + 1
-            btic = time.time()
+        iter = iter + 1
+        btic = time.time()
 
         name, acc = metric.get()
         metric.reset()
@@ -221,7 +192,7 @@ def train(pool_size, epochs, train_data, val_data,  ctx, netEn, netDe,  netD, tr
         logging.info('\nbinary training acc at epoch %d: %s=%f' % (epoch, name, acc))
         logging.info('time: %f' % (time.time() - tic))
         if epoch%10 ==0:
-	    text_file = open(expname + "_validtest.txt", "a")
+            text_file = open(expname + "_validtest.txt", "a")
             filename = "checkpoints/"+expname+"_"+str(epoch)+"_D.params"
             netD.save_params(filename)
             filename = "checkpoints/"+expname+"_"+str(epoch)+"_G.params"
@@ -232,12 +203,12 @@ def train(pool_size, epochs, train_data, val_data,  ctx, netEn, netDe,  netD, tr
             fake_img4 = nd.concat(real_in[3],real_out[3], fake_out[3], dim=1)
             val_data.reset()
            
-	    for vbatch in val_data:
+        for vbatch in val_data:
                 
-            	real_in = vbatch.data[0].as_in_context(ctx)
-            	real_out = vbatch.data[1].as_in_context(ctx)
-            	fake_out = netG(real_in)
-                metric2.update([fake_out, ], [real_out, ])
+            real_in = vbatch.data[0].as_in_context(ctx)
+            real_out = vbatch.data[1].as_in_context(ctx)
+            fake_out = netG(real_in)
+            metric2.update([fake_out, ], [real_out, ])
             _, acc2 = metric2.get()
             text_file.write("%s %s %s\n" % (str(epoch), nd.mean(errR).asscalar(), str(acc2)))
             metric2.reset()
@@ -249,10 +220,7 @@ def train(pool_size, epochs, train_data, val_data,  ctx, netEn, netDe,  netD, tr
             fake_img = nd.concat(fake_img1,fake_img2, fake_img3,fake_img1T,fake_img2T, fake_img3T,dim=2)
             visual.visualize(fake_img)
             plt.savefig('outputs/'+expname+'_'+str(epoch)+'.png')
-    	    text_file.close()
-
-        
-            
+            text_file.close()
     return([loss_rec_D,loss_rec_G, loss_rec_R, acc_rec])
 
 
@@ -270,9 +238,9 @@ def print_result():
 
 def main(opt): 
     if opt.useAE == 1:
-	useAE = True
+        useAE = True
     else:
-	useAE = False  
+        useAE = False
     if opt.seed != -1:
             random.seed(opt.seed)
     ctx = mx.gpu() if opt.use_gpu else mx.cpu()
@@ -281,102 +249,90 @@ def main(opt):
     print('Data loading done.')
 
     if opt.istest:
-	    testclasspaths = []
-	    testclasslabels = []
-	    if opt.istest:
-	        filename = '_testlist.txt'
-	    elif opt.isvalidation:
-	        filename = '_trainlist.txt'
-	    else:
-	        filename = '_validationlist.txt'
+        testclasspaths = []
+        testclasslabels = []
+        if opt.istest:
+            filename = '_testlist.txt'
+        elif opt.isvalidation:
             filename = '_trainlist.txt'
-	    with open(opt.dataset+"_"+opt.expname+filename , 'r') as f:
-	        for line in f:
-	            testclasspaths.append(line.split(' ')[0])
-	            if int(line.split(' ')[1])==-1:
-	                testclasslabels.append(0)
-	            else:
-	                testclasslabels.append(1)
+        else:
+            filename = '_validationlist.txt'
+            filename = '_trainlist.txt'
+        with open(opt.dataset+"_"+opt.expname+filename , 'r') as f:
+            for line in f:
+                testclasspaths.append(line.split(' ')[0])
+                if int(line.split(' ')[1])==-1:
+                    testclasslabels.append(0)
+                else:
+                    testclasslabels.append(1)
 
-	    test_data = load_image.load_test_images(testclasspaths,testclasslabels,opt.batch_size, opt.img_wd, opt.img_ht, ctx, opt.noisevar)
-	    netG, netD, trainerG, trainerD = set_network(opt.depth, ctx, 0, 0, opt.ndf, opt.ngf, opt.append)
-	    netG.load_params('checkpoints/'+opt.expname+'_'+str(opt.epochs)+'_G.params', ctx=ctx)
-	    netD.load_params('checkpoints/'+opt.expname+'_'+str(opt.epochs)+'_D.params', ctx=ctx)
-	    lbllist = [];
-	    scorelist1 = [];
-	    scorelist2 = [];
-	    scorelist3 = [];
-	    scorelist4 = [];
-	    test_data.reset()
-	    count = 0
+        test_data = load_image.load_test_images(testclasspaths,testclasslabels,opt.batch_size, opt.img_wd, opt.img_ht, ctx, opt.noisevar)
+        netG, netD, trainerG, trainerD = set_network(opt.depth, ctx, 0, 0, opt.ndf, opt.ngf, opt.append)
+        netG.load_params('checkpoints/'+opt.expname+'_'+str(opt.epochs)+'_G.params', ctx=ctx)
+        netD.load_params('checkpoints/'+opt.expname+'_'+str(opt.epochs)+'_D.params', ctx=ctx)
+        lbllist = [];
+        scorelist1 = [];
+        scorelist2 = [];
+        scorelist3 = [];
+        scorelist4 = [];
+        test_data.reset()
+        count = 0
 
-	    for batch in (test_data):
-        	count+=1
-        	real_in = batch.data[0].as_in_context(ctx)
-        	real_out = batch.data[1].as_in_context(ctx)
-        	lbls = batch.label[0].as_in_context(ctx)
-        	outnn = (netG(real_out))
-        	out_concat =  nd.concat(real_out, outnn, dim=1) if opt.append else  outnn
-        	output4 = nd.mean((netD(out_concat)), (1, 3, 2)).asnumpy()
-        	out = (netG(real_in))
-        	out_concat =  nd.concat(real_in, out, dim=1) if opt.append else  out
-        	output = netD(out_concat) #Denoised image
-        	output3 = nd.mean(out-real_out, (1, 3, 2)).asnumpy() #denoised-real
-        	output = nd.mean(output, (1, 3, 2)).asnumpy()
-        	out_concat =  nd.concat(real_out, real_out, dim=1) if opt. append else  real_out
-        	output2 = netD(out_concat) #Image with no noise
-        	output2 = nd.mean(output2, (1, 3, 2)).asnumpy()
-        	lbllist = lbllist+list(lbls.asnumpy())
-        	scorelist1 = scorelist1+list(output)
-        	scorelist2 = scorelist2+list(output2)
-        	scorelist3 = scorelist3+list(output3)
-        	scorelist4 = scorelist4+list(output4)
+        for batch in (test_data):
+            count+=1
+            real_in = batch.data[0].as_in_context(ctx)
+            real_out = batch.data[1].as_in_context(ctx)
+            lbls = batch.label[0].as_in_context(ctx)
+            outnn = (netG(real_out))
+            out_concat =  nd.concat(real_out, outnn, dim=1) if opt.append else  outnn
+            output4 = nd.mean((netD(out_concat)), (1, 3, 2)).asnumpy()
+            out = (netG(real_in))
+            out_concat =  nd.concat(real_in, out, dim=1) if opt.append else  out
+            output = netD(out_concat) #Denoised image
+            output3 = nd.mean(out-real_out, (1, 3, 2)).asnumpy() #denoised-real
+            output = nd.mean(output, (1, 3, 2)).asnumpy()
+            out_concat =  nd.concat(real_out, real_out, dim=1) if opt. append else  real_out
+            output2 = netD(out_concat) #Image with no noise
+            output2 = nd.mean(output2, (1, 3, 2)).asnumpy()
+            lbllist = lbllist+list(lbls.asnumpy())
+            scorelist1 = scorelist1+list(output)
+            scorelist2 = scorelist2+list(output2)
+            scorelist3 = scorelist3+list(output3)
+            scorelist4 = scorelist4+list(output4)
 
-   	    fake_img1 = nd.concat(real_in[0],real_out[0], out[0], outnn[0],dim=1)
-   	    fake_img2 = nd.concat(real_in[1],real_out[1], out[1],outnn[1], dim=1)
-    	    fake_img3 = nd.concat(real_in[2],real_out[2], out[2], outnn[2], dim=1)
-       	    fake_img4 = nd.concat(real_in[3],real_out[3],out[3],outnn[3], dim=1)
+        fake_img1 = nd.concat(real_in[0],real_out[0], out[0], outnn[0],dim=1)
+        fake_img2 = nd.concat(real_in[1],real_out[1], out[1],outnn[1], dim=1)
+            fake_img3 = nd.concat(real_in[2],real_out[2], out[2], outnn[2], dim=1)
+            fake_img4 = nd.concat(real_in[3],real_out[3],out[3],outnn[3], dim=1)
             fake_img = nd.concat(fake_img1,fake_img2, fake_img3,fake_img4, dim=2)
-      	    #print(np.shape(fake_img))
+            #print(np.shape(fake_img))
        	    visual.visualize(fake_img)
-       	    plt.savefig('outputs/T_'+opt.expname+'_'+str(count)+'.png')
-
-   
-	    '''
-            fpr, tpr, _ = roc_curve(lbllist, scorelist1, 1)
-            roc_auc1 = auc(fpr, tpr)
-            fpr, tpr, _ = roc_curve(lbllist, scorelist2, 1)
-            roc_auc2 = auc(fpr, tpr)
-            fpr, tpr, _ = roc_curve(lbllist, scorelist3, 1)
-            roc_auc3 = auc(fpr, tpr)
-            fpr, tpr, _ = roc_curve(lbllist, scorelist4, 1)
-            roc_auc4 = auc(fpr, tpr)
-            return([roc_auc1, roc_auc2, roc_auc3, roc_auc4])
-            '''
-
-	    return ([0,0,0,0])                                                                                             
+            plt.savefig('outputs/T_'+opt.expname+'_'+str(count)+'.png')
 
 
-    else:	
-	    netEn, netDe,  netD, trainerEn, trainerDe, trainerD = set_network(opt.depth, ctx, opt.lr, opt.beta1,opt.ndf,  opt.ngf, opt.append)
-	    if opt.graphvis:
-	        print(netG)
-	    print('training')
-	    print(opt.epochs)
-	    loss_vec = train(opt.pool_size, opt.epochs, train_data,val_data, ctx, netEn, netDe,  netD, trainerEn, trainerDe, trainerD, opt.lambda1, opt.batch_size, opt.expname,  opt.append, useAE = useAE)
-	    plt.gcf().clear()
-	    plt.plot(loss_vec[0], label="D", alpha = 0.7)
-	    plt.plot(loss_vec[1], label="G", alpha=0.7)
-	    plt.plot(loss_vec[2], label="R", alpha= 0.7)
-	    plt.plot(loss_vec[3], label="Acc", alpha = 0.7)
-	    plt.legend()
-	    plt.savefig('outputs/'+opt.expname+'_loss.png')
-	    return inclasses
+        return ([0,0,0,0])
+
+
+    else:
+        netEn, netDe,  netD, trainerEn, trainerDe, trainerD = set_network(opt.depth, ctx, opt.lr, opt.beta1,opt.ndf,  opt.ngf, opt.append)
+        if opt.graphvis:
+            print(netG)
+        print('training')
+        print(opt.epochs)
+        loss_vec = train(opt.pool_size, opt.epochs, train_data,val_data, ctx, netEn, netDe,  netD, trainerEn, trainerDe, trainerD, opt.lambda1, opt.batch_size, opt.expname,  opt.append, useAE = useAE)
+        plt.gcf().clear()
+        plt.plot(loss_vec[0], label="D", alpha = 0.7)
+        plt.plot(loss_vec[1], label="G", alpha=0.7)
+        plt.plot(loss_vec[2], label="R", alpha= 0.7)
+        plt.plot(loss_vec[3], label="Acc", alpha = 0.7)
+        plt.legend()
+        plt.savefig('outputs/'+opt.expname+'_loss.png')
+        return inclasses
 
 if __name__ == "__main__":
-    opt = options.train_options()     
+    opt = options.train_options()
     inclasses = main(opt)
     text_file1 = open("classnames.txt","a")
     text_file1.write("%s \n" % (inclasses[0]))
-    text_file1.close()    
-#print("Training finished for "+ inclasses)
+    text_file1.close()
+    #print("Training finished for "+ inclasses)
