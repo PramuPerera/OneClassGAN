7b01c186e1e1222e51a344738404d1c1e8c09c43
Perera <pramudi@8c8590432a82.ant.amazon.com>
2018-07-02 16:35:35 -0400

    testing added

---
+++ TestNovelty.py
@@ -0,0 +1,133 @@
+#https://github.com/zackchase/mxnet-the-straight-dope/blob/master/chapter14_generative-adversarial-networks/pixel2pixel.ipynb
+from __future__ import print_function
+import os
+import matplotlib as mpl
+import tarfile
+import matplotlib.image as mpimg
+from matplotlib import pyplot as plt
+import mxnet as mx
+from mxnet import gluon
+from mxnet import ndarray as nd
+from mxnet.gluon import nn, utils
+from mxnet.gluon.nn import Dense, Activation, Conv2D, Conv2DTranspose, \
+    BatchNorm, LeakyReLU, Flatten, HybridSequential, HybridBlock, Dropout
+from mxnet import autograd
+import numpy as np
+from random import shuffle
+from sklearn.metrics import roc_curve, auc
+import load_image
+import models
+from datetime import datetime
+import time
+import logging
+import options
+import visual
+import random
+import argparse
+#logging.basicConfig()
+
+
+
+def facc(label, pred):
+    pred = pred.ravel()
+    label = label.ravel()
+    return ((pred > 0.5) == label).mean()
+
+
+def main(opt):
+    ctx = mx.gpu() if opt.use_gpu else mx.cpu()
+    testclasspaths = []
+    testclasslabels = []
+    print('loading test files')
+    filename = '_trainlist.txt'
+    with open(opt.dataset+"_"+opt.expname+filename , 'r') as f:
+        for line in f:
+            testclasspaths.append(line.split(' ')[0])
+            if int(line.split(' ')[1]) == -1:
+                testclasslabels.append(0)
+            else:
+                testclasslabels.append(1)
+    neworder = range(len(testclasslabels))
+    c = list(zip(testclasslabels, testclasspaths))
+    print('shuffling')
+    random.shuffle(c)
+    testclasslabels, testclasspaths = zip(*c)
+    testclasslabels = testclasslabels[1:5000]
+    testclasspaths = testclasspaths[1:5000]
+    print('loading pictures')
+    test_data = load_image.load_test_images(testclasspaths,testclasslabels,opt.batch_size, opt.img_wd, opt.img_ht, ctx, opt.noisevar)
+    print('picture loading done')
+    opt.istest = True
+    networks = models.set_network(opt, ctx)
+    netEn = networks[0]
+    netDe = networks[1]
+    netD = networks[2]
+    netD2 = networks[3]
+    print('Model loading done')
+    lbllist = [];
+    scorelist1 = [];
+    scorelist2 = [];
+    scorelist3 = [];
+    scorelist4 = [];
+    test_data.reset()
+    count = 0
+    for batch in (test_data):
+        output1=0
+        output2=0
+        output3=0
+        output4=0
+        real_in = batch.data[0].as_in_context(ctx)
+        real_out = batch.data[1].as_in_context(ctx)
+        lbls = batch.label[0].as_in_context(ctx)
+        outnn = (netDe(netEn((real_in))))
+        output3 = nd.mean((outnn - real_out)**2, (1, 3, 2)).asnumpy()
+        if opt.ntype >1: #AE
+            out_concat = nd.concat(real_in, outnn, dim=1) if opt.append else outnn
+            output1 = nd.mean((netD(out_concat)), (1, 3, 2)).asnumpy()
+            out_concat = nd.concat(real_in, real_in, dim=1) if opt.append else real_in
+            output2 = netD2(netEn(out_concat))  # Image with no noise
+            output2 = nd.mean(output2, (1)).asnumpy()
+            out = netDe(netEn(real_out))
+            out_concat =  nd.concat(real_in, out, dim=1) if opt.append else out
+            output = netD(out_concat) #Denoised image
+            output4 = nd.mean(output, (1, 3, 2)).asnumpy()
+
+        lbllist = lbllist+list(lbls.asnumpy())
+        scorelist1 = scorelist1+list(output1)
+        scorelist2 = scorelist2+list(output2)
+        scorelist3 = scorelist3+list(output3)
+        scorelist4 = scorelist4+list(output4)
+
+        fake_img1 = nd.concat(real_in[0],real_out[0], out[0], outnn[0],dim=1)
+        fake_img2 = nd.concat(real_in[1],real_out[1], out[1],outnn[1], dim=1)
+        fake_img3 = nd.concat(real_in[2],real_out[2], out[2], outnn[2], dim=1)
+        fake_img4 = nd.concat(real_in[3],real_out[3],out[3],outnn[3], dim=1)
+        fake_img = nd.concat(fake_img1,fake_img2, fake_img3,fake_img4, dim=2)
+        #print(np.shape(fake_img))
+        visual.visualize(fake_img)
+        plt.savefig('outputs/T_'+opt.expname+'_'+str(count)+'.png')
+    print(np.shape(scorelist1))
+    print(np.shape(lbllist))
+    fpr, tpr, _ = roc_curve(lbllist, scorelist1, 1)
+    roc_auc1 = auc(fpr, tpr)
+    fpr, tpr, _ = roc_curve(lbllist, scorelist2, 1)
+    roc_auc2 = auc(fpr, tpr)
+    fpr, tpr, _ = roc_curve(lbllist, scorelist3, 1)
+    roc_auc3 = auc(fpr, tpr)
+    fpr, tpr, _ = roc_curve(lbllist, scorelist4, 1)
+    roc_auc4 = auc(fpr, tpr)
+    fakecode = nd.random_normal(loc=0, scale=1, shape=(16, opt.latent, 1 ,1), ctx=ctx)
+    out = netDe(fakecode)
+    fake_img1 = nd.concat(out[0],out[1], out[2], out[3],dim=1)
+    fake_img2 = nd.concat(out[7],out[6], out[5], out[4],dim=1)
+    fake_img3 = nd.concat(out[8],out[9], out[10], out[11],dim=1)
+    fake_img4 = nd.concat(out[15],out[14], out[13], out[12],dim=1)
+    fake_img = nd.concat(fake_img1,fake_img2, fake_img3,fake_img4, dim=2)
+    visual.visualize(fake_img)
+    plt.savefig('outputs/fakes_'+opt.expname+'_.png')
+    return([roc_auc1, roc_auc2, roc_auc3, roc_auc4])
+
+if __name__ == "__main__":
+    opt = options.test_options()
+    roc_auc = main(opt)
+    print(roc_auc)
